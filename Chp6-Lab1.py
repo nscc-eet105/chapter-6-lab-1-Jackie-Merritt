94bd3494851fce5c058eabf58b27be5c42fc6b00
Jackie Merritt <remmerritt@gmail.com>
2025-07-01 15:36:13 -0400

    Assignment completed

--- Chp6-Lab1.py
+++ Chp6-Lab1.py
@@ -10,8 +10,8 @@ def main():
 
     print(f'\nTotal weekly sales    : $ {total:.2f}')
     print(f'Average weekly sales  : $ {average:.2f}')
-    print(f'\nThe highest sales were ${max(ammount_list):.2f} on ')
-    print(f'The lowest sales were ${min(ammount_list):.2f} on ')
+    print(f'\nThe highest sales were ${max(ammount_list):.2f} on {days[ammount_list.index(max(ammount_list))]}')
+    print(f'The lowest sales were ${min(ammount_list):.2f} on {days[ammount_list.index(min(ammount_list))]}')
 
 
 def weekly_sales():
@@ -28,6 +28,7 @@ def calculate_total(ammount_list):
     return total
 
 def calculate_average(total):
-    return total / 7
+    return total / 7 
 
-main()
+if __name__ == '__main__':
+    main()
